25c9d22984ee6eb783269af96232e24991ec7e51
Max Haze <mahan.hazrati@iflix.com>
2018-12-04 00:58:34 +0800

    backup

---
+++ README.md
@@ -0,0 +1,7 @@
+## install
+```
+virtualenv venv --python=python3
+source venv/bin/activate
+
+pip install -r requirements.txt
+```
---
+++ backup.py
@@ -0,0 +1,30 @@
+import json
+
+from git import Repo
+from github import Github
+
+org = '<org>'
+
+git = Github('<token>')
+
+git.per_page = 1
+
+git_org = git.get_organization(org)
+git_repos = git_org.get_repos('source')
+
+total_count = git_repos.totalCount
+
+def progress(*args):
+ print(args, flush=True)
+
+def clone(repo):
+  path = 'git clone %s' % (repo.ssh_url) 
+  print(path)
+  Repo.clone_from(repo.ssh_url, path, progress)
+
+def main ():
+  for repo in git_repos:
+    clone(repo)
+  
+if __name__ == '__main__':
+  main()
